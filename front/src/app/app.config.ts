0d931f05afc28233cc6dc8999b7f93de37088f28
Julien Marcou <julien.marcou.dev@gmail.com>
2025-12-11 16:57:19 +0100

    Upgrade Angular to v21

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import type { ApplicationConfig } from '@angular/core';
 import type { Routes } from '@angular/router';
 
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { PuzzlePreviewComponent } from './components/puzzle-preview/puzzle-preview.component';
@@ -19,6 +19,7 @@ const APP_ROUTES: Routes = [
 
 export const APP_CONFIG: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(APP_ROUTES),
   ],
